44afac40f8fc5e1bc143b99cf44f0e175db0f9f2
RagahTzy <ragahediting@gmail.com>
2026-04-27 10:16:35 +0700

    nambahin UI bagian ragah

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,10 @@
+<table class="w-full bg-card rounded-xl overflow-hidden">
+    <thead class="bg-secondary text-left text-sm">
+        <tr>
+            {{ $head }}
+        </tr>
+    </thead>
+    <tbody class="text-sm">
+        {{ $slot }}
+    </tbody>
+</table>
